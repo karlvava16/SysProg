1934378412abdbc66e5d893081cbea63b0a67b24
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2022-11-10 01:26:12 +0200

    added project settings files

---
+++ 09.11.2022/Main.cpp
@@ -0,0 +1,36 @@
+#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+HICON hIcon;
+HBITMAP hbPictures[2];
+HWND hFields[9];
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+	// создаём главное окно приложения на основе модального диалога
+	return  DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp)
+{
+
+	switch (message)
+	{
+	case WM_INITDIALOG:
+	{
+
+	}
+	break;
+	case WM_COMMAND:
+	{
+
+	}
+	break;
+	case WM_CLOSE:
+		EndDialog(hWnd, 0); // закрываем модальный диалог
+		return TRUE;
+	}
+	return FALSE;
+}
