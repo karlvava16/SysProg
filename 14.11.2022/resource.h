699d900978491f0b92add5714601cba3ee6c2038
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2022-11-14 14:07:29 +0200

    14.11.2022

---
+++ 14.11.2022/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 14.11.2022.rc
+//
+#define IDD_DIALOG1                     101
+#define IDD_DIALOG2                     103
+#define IDD_DIALOG3                     104
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define IDC_EDIT1                       1004
+#define IDC_EDIT2                       1005
+#define IDC_EDIT3                       1006
+#define IDC_COMBO1                      1007
+#define IDC_EDIT6                       1007
+#define IDC_RADIO1                      1008
+#define IDC_EDIT8                       1008
+#define IDC_RADIO2                      1009
+#define IDC_EDIT9                       1009
+#define IDC_EDIT4                       1010
+#define IDC_EDIT5                       1011
+#define IDC_EDIT10                      1011
+#define IDC_EDIT7                       1013
+#define IDC_CHECK1                      1014
+#define IDC_CHECK2                      1015
+#define IDC_CHECK3                      1016
+#define IDC_CHECK4                      1017
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
