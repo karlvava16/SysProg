62394e85271ec0193a7df1c6b185d4056a826a7d
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2022-11-29 11:22:49 +0200

    [BlindTyping] added interface

---
+++ BlindTypingTraining/BlindTypingTraining/resource.h
@@ -0,0 +1,82 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by BlindTypingTraining.rc
+//
+#define IDD_DIALOG1                     101
+#define IDR_MENU1                       103
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define IDC_BUTTON3                     1003
+#define IDC_BUTTON4                     1008
+#define IDC_BUTTON5                     1009
+#define IDC_BUTTON6                     1010
+#define IDC_BUTTON7                     1011
+#define IDC_BUTTON8                     1012
+#define IDC_BUTTON9                     1013
+#define IDC_BUTTON10                    1014
+#define IDC_BUTTON11                    1015
+#define IDC_BUTTON12                    1026
+#define IDC_BUTTON13                    1027
+#define IDC_BUTTON14                    1028
+#define IDC_BUTTON15                    1029
+#define IDC_BUTTON16                    1030
+#define IDC_BUTTON17                    1031
+#define IDC_BUTTON18                    1032
+#define IDC_BUTTON19                    1033
+#define IDC_BUTTON20                    1034
+#define IDC_BUTTON21                    1035
+#define IDC_BUTTON22                    1042
+#define IDC_BUTTON24                    1044
+#define IDC_BUTTON25                    1045
+#define IDC_BUTTON26                    1046
+#define IDC_BUTTON27                    1047
+#define IDC_BUTTON28                    1048
+#define IDC_BUTTON29                    1049
+#define IDC_BUTTON30                    1050
+#define IDC_BUTTON31                    1051
+#define IDC_BUTTON32                    1052
+#define IDC_BUTTON33                    1053
+#define IDC_BUTTON34                    1054
+#define IDC_BUTTON35                    1055
+#define IDC_BUTTON36                    1056
+#define IDC_BUTTON23                    1057
+#define IDC_BUTTON37                    1059
+#define IDC_BUTTON39                    1061
+#define IDC_BUTTON42                    1064
+#define IDC_BUTTON43                    1065
+#define IDC_BUTTON44                    1066
+#define ID_LEVELS_1LEVEL                40001
+#define ID_LEVELS_2LEVEL                40002
+#define ID_LEVELS_3LEVEL                40003
+#define ID_BASELEVEL_1LEVEL             40004
+#define ID_BASELEVEL_2LEVEL             40005
+#define ID_BASELEVEL_3LEVEL             40006
+#define ID_BASELEVEL_4LEVEL             40007
+#define ID_LEVELS_NORMALLEVELS          40008
+#define ID_NORMALLEVELS_5LEVEL          40009
+#define ID_NORMALLEVELS_6LEVEL          40010
+#define ID_NORMALLEVELS_8LEVEL          40011
+#define ID_NORMALLEVELS_9LEVEL          40012
+#define ID_NORMALLEVELS_7LEVEL          40013
+#define ID_MENU                         40014
+#define ID_LEVELS_HARDLEVELS            40015
+#define ID_HARDLEVELS_9LEVEL            40016
+#define ID_HARDLEVELS_10LEVEL           40017
+#define ID_HARDLEVELS_11LEVEL           40019
+#define ID_HARDLEVELS_12LEVEL           40020
+#define ID_LEVELS_BLINDTYPINGLEVEL      40021
+#define ID_MENU_START                   40023
+#define ID_MENU_PAUSE                   40024
+#define ID_MENU_RESTART                 40025
+#define ID_MENU_EXIT                    40026
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40027
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
